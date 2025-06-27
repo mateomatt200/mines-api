cdd6c97c352f274e3291f4bb4e0c7cdc8d607abf
The Holiday <dragonlatam704@gmail.com>
2025-06-27 18:21:38 -0500

    Update

--- src/main/java/com/venned/simplemines/api/IMine.java
+++ src/main/java/com/venned/simplemines/api/IMine.java
@@ -1,7 +1,10 @@
 package com.venned.simplemines.api;
 
+import org.bukkit.Location;
+
 public interface IMine {
     String getName();
+    boolean isInside(Location location);
     void reset();
     long getRemainingTime();
 }
--- src/main/java/com/venned/simplemines/api/MineAPI.java
+++ src/main/java/com/venned/simplemines/api/MineAPI.java
@@ -1,12 +1,16 @@
 package com.venned.simplemines.api;
 
 
+import org.bukkit.Location;
+
 import java.util.Map;
 
 public interface MineAPI {
     Map<String, IMine> getMines();
 
     IMine getMine(String name);
+
+    String getMineNameAt(Location loc);
 
     boolean resetMine(String name);
 
